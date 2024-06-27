dc70d98dc2a9830b1d168535e2a39b18797a3c96
Jean claro dos Santos <jeanclarodev@gmail.com>
2024-06-26 22:50:48 -0300

    config feitas

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/uma-homenagem-para-mari",
+  base: "/aniversario-de-namoro",
   plugins: [react()],
 });
